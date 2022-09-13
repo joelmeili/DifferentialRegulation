1116da65cf375cd0930063170f7e90e7f5d424f7
Joël Meili <joel.meili@uzh.ch>
2022-09-13 21:35:29 +0200

    updates

--- code/02_exploratory_analysis/qc_plots.R
+++ code/02_exploratory_analysis/qc_plots.R
@@ -1,21 +1,32 @@
-suppressPackageStartupMessages(
+suppressPackageStartupMessages({
   library(scater)
-)
+  library(gridExtra)
+})
 
-# remove undetected genes
-sce <- sce[rowSums(assays(sce)$TOT_counts > 0) > 0, ]
+# load unfiltered sce
+sce <- readRDS("kidney_mouse/03_data/mouse_data_fry_USA_no_QC.rds")
 
 # calculate per-cell quality control (QC) metrics
 qc <- perCellQCMetrics(sce)
 
 # remove cells with few or many detected genes
 ol <- isOutlier(metric = qc$detected, nmads = 2, log = TRUE)
-sce <- sce[, !ol]
+qc <- cbind(qc, ol)
+
+colData(sce) <- cbind(colData(sce), qc)
+sce$sample_id <- factor(sce$sample_id)
+
+# remove undetected genes
+sce_filtered <- sce[rowSums(assays(sce)$TOT_counts > 0) > 0, ]
 
 # remove lowly expressed genes: at least 10 non-zero cells:
-sce <- sce[rowSums(assays(sce)$TOT_counts > 0) >= 10, ]
+sce_filtered <- sce_filtered[rowSums(assays(sce_filtered)$TOT_counts > 0) >= 10, ]
+sce_filtered <- sce_filtered[, !ol]
 
+# diagnostic plot comparing counts before and after QC 
+g1 <- plotColData(sce, x = "sample_id", y = "sum", colour_by = "ol") + scale_y_log10() +
+  guides(colour = guide_legend(title = "Outlier")) + xlab("Sample id")
+g2 <- plotColData(sce_filtered, x = "sample_id", y = "sum") + scale_y_log10() + xlab("Sample id")
 
-
-plotColData(sce, x = "block", y = "detected", colour_by = "discard",
-            other_fields = "sample_id") + facet_wrap(~sample_id) 
+g <- arrangeGrob(g1, g2, nrow = 2)
+ggsave(g, file = "kidney_mouse/04_figures/qc_mouse.png", height = 4, width = 6)
