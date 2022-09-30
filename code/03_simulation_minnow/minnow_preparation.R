ae1346ae33b5a301c62ae8d237b0be6be35b89f7
Joël Meili <joel.meili@uzh.ch>
2022-09-30 16:55:14 +0200

    minor changes

--- code/03_simulation_minnow/minnow_preparation.R
+++ code/03_simulation_minnow/minnow_preparation.R
@@ -5,28 +5,28 @@ suppressPackageStartupMessages({
 rm(list = ls())
 
 sim_dir <- "kidney_mouse/04_simulation_minnow/simulation/"
-sim_dir_DGE <- "simulation/04_simulation_minnow/simulation_DGE/"
+sim_dir_DGE <- "kidney_mouse/04_simulation_minnow/simulation_DGE/"
 
 sce <- readRDS("kidney_mouse/03_data/mouse_simulation_data.rds")
 sce_DGE <- readRDS("kidney_mouse/03_data/mouse_simulation_DGE_data.rds")
 
-cts <- c("Adipocytes", "Epithelial cells", "Hepatocytes")
+cell_types <- c("Adipocytes", "Epithelial cells", "Hepatocytes")
 
 prepare_minnow <- function (sce, sim_dir) {
-  for (samp in 1:4) {
-    for (ct in cts) {
-      temp <- sce[, sce$sample_id == samp & sce$cell_type == ct]
+  for (sample_id in 1:4) {
+    for (cell_type in cell_types) {
+      temp <- sce[, sce$sample_id == sample_id & sce$cell_type == cell_type]
       
       gnames <- rownames(temp)
-      bcs <- sub(paste0(samp, "."), "", colnames(temp))
+      bcs <- sub(paste0(sample_id, "."), "", colnames(temp))
       
-      ct <- ifelse(ct == "Epithelial cells", "Epithelial_cells", ct)
+      ct <- ifelse(cell_type == "Epithelial cells", "Epithelial_cells", cell_type)
       
       s <- assay(temp, "spliced")
       u <- assay(temp, "unspliced")
       
-      pat <- paste0(samp, ".")
-      out_dir <- file.path(sim_dir, paste0("normal", samp), ct)
+      pat <- paste0(sample_id, ".")
+      out_dir <- file.path(sim_dir, paste0("normal", sample_id), cell_type)
       dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
       
       colnames(s) <- sub(pat, "", colnames(s))
