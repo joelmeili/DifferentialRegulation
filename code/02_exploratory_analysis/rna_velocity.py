3a876681cb204e8dc75200701a1dc4c254755ec5
Joël Meili <joel.meili@uzh.ch>
2022-09-30 14:47:15 +0200

    update to rna velocity code

--- code/02_exploratory_analysis/rna_velocity.py
+++ code/02_exploratory_analysis/rna_velocity.py
@@ -1,38 +1,99 @@
 import scvelo as scv
 import matplotlib
 import matplotlib.pyplot as plt
-matplotlib.use("AGG")
 
+matplotlib.use("AGG")
 scv.logging.print_version()
 
 scv.settings.verbosity = 3
 scv.settings.presenter_view = True
-scv.set_figure_params("scvelo")
+scv.set_figure_params("scvelo", fontsize = 6)
 
 fig_path = "figures/kidney_mouse/"
 
-for i in range(1, 5):
+for i in range(1, 2):
     adata = scv.read("kidney_mouse/03_data/adata_normal" + str(i) + ".h5ad", cache = True)
     adata.layers["spliced"] = adata.X
+    adata.obs = adata.obs.astype("category")
 
-    # show spliced and unspliced proportion
-    scv.pl.proportions(adata)
-    plt.savefig(fname = fig_path + "proportions_normal" + str(i) + ".png")
+    temp_path = fig_path + "normal" + str(i) + "_"
 
-    # basic preprocessing
+    #########################################################
+    # Basic preprocessing:
+    #########################################################
     scv.pp.filter_and_normalize(adata, enforce = True)
-    scv.pp.normalize_per_cell(adata, enforce = True)
-    scv.pp.filter_genes_dispersion(adata, n_top_genes = 2000)
-    scv.pp.log1p(adata)
-    scv.pp.filter_and_normalize(adata, min_shared_counts = 20, n_top_genes = 2000, enforce = True)
-    scv.pp.moments(adata, n_pcs = 30, n_neighbors = 30)
-
-    # estimate RNA velocity
-    scv.tl.velocity(adata)
+    scv.pp.moments(adata)
+    
+    #########################################################
+    # Plot proportions of spliced and unspliced reads:
+    #########################################################   
+    scv.pl.proportions(adata)
+    plt.savefig(temp_path + "proportions.png", dpi = 300, bbox_inches = "tight")
+    
+    #########################################################
+    # Velocity tools:
+    #########################################################  
+    scv.tl.recover_dynamics(adata, n_jobs = 32)
+    scv.tl.velocity(adata, mode = "dynamical")
     scv.tl.velocity_graph(adata)
-    plt.savefig(fname = fig_path + "velocity_graph_normal" + str(i) + ".png")
 
-    # visualize RNA velocity
-    scv.pl.velocity_embedding_stream(adata, basis = "UMAP")
-    scv.pl.velocity_embedding(adata, arrow_length = 3, arrow_size = 2, dpi = 120)
-    plt.savefig(fname = fig_path + "velocity_umap_normal" + str(i) + ".png")
+    scv.pl.velocity_embedding_stream(adata, basis = "umap")
+    plt.savefig(temp_path + "dynamical_model_combined.png", dpi = 300, bbox_inches = "tight")
+
+    #########################################################
+    # Velocity pseudotime:
+    #########################################################
+    scv.pl.velocity_graph(adata, threshold = 0.1)
+    plt.savefig(temp_path + "velocity_threshold.png", dpi = 300, bbox_inches = "tight")
+    
+    x, y = scv.utils.get_cell_transitions(adata, basis = "umap")
+    ax = scv.pl.velocity_graph(adata, c = "lightgrey", edge_width = 0.05, show = False)
+    ax = scv.pl.scatter(adata, x = x, y = y, s = 120, c = "ascending", cmap = "gnuplot", ax = ax)
+    plt.savefig(temp_path + "cell_transitions.png", dpi = 300, bbox_inches = "tight")
+    
+    scv.tl.velocity_pseudotime(adata)
+    scv.pl.scatter(adata, color = "velocity_pseudotime", cmap = "gnuplot")
+    plt.savefig(temp_path + "velocity_pseudo_time_graph.png", dpi = 300, bbox_inches = "tight")
+    
+    #########################################################
+    # latent-time graph:
+    #########################################################
+    scv.tl.latent_time(adata)
+    scv.pl.scatter(adata, color = "latent_time", color_map = "gnuplot", size = 80)
+    plt.savefig(temp_path + "velocity_latent_time_graph.png", dpi = 300, bbox_inches = "tight")
+    
+    #########################################################
+    # PAGA:
+    #########################################################
+    adata.uns["neighbors"]["distances"] = adata.obsp["distances"]
+    adata.uns["neighbors"]["connectivities"] = adata.obsp["connectivities"]
+    
+    scv.tl.paga(adata, groups = "clusters")
+    df = scv.get_df(adata, "paga/transitions_confidence", precision = 2).T
+    df.style.background_gradient(cmap = "Blues").format("{:.2g}")
+    
+    scv.pl.paga(adata, basis = "umap", size = 50, alpha = 0.1,
+                min_edge_width = 2, node_size_scale = 1.5)
+                
+    plt.savefig(temp_path + "PAGA.png", dpi = 300, bbox_inches = "tight")
+    
+    #########################################################
+    # Speed and coherence:
+    #########################################################
+    scv.tl.velocity_confidence(adata)
+    keys = "velocity_length", "velocity_confidence"
+    scv.pl.scatter(adata, c = keys, cmap = "coolwarm", perc = [5, 95])
+    plt.savefig(temp_path + "speed.png", dpi = 300, bbox_inches = "tight")
+    
+    #########################################################
+    # Calculate average velocity:
+    #########################################################
+    velo = adata.layers["velocity"] # cells are rows, genes are columns
+    velo = abs(velo)
+    clusters = adata.obs["clusters"].values
+    
+    velo = pd.DataFrame(velo)
+    velo["clusters"] = clusters
+    
+    avg_velo = velo.groupby("clusters").mean().transpose()
+    avg_velo.to_csv("kidney_mouse/03_data/" + "avg_velo_sample" + str(i) + ".csv")
