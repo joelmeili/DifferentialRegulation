c9d4788e2672ad98f897540e3c31adab8281b445
Joël Meili <joel.meili@uzh.ch>
2022-09-30 14:39:08 +0200

    updated the velocity code

--- code/02_exploratory_analysis/rna_velocity.py
+++ code/02_exploratory_analysis/rna_velocity.py
@@ -11,7 +11,7 @@ scv.set_figure_params("scvelo")
 
 fig_path = "figures/kidney_mouse/"
 
-for i in range(1, 2):
+for i in range(1, 5):
     adata = scv.read("kidney_mouse/03_data/adata_normal" + str(i) + ".h5ad", cache = True)
     adata.layers["spliced"] = adata.X
 
@@ -24,14 +24,15 @@ for i in range(1, 2):
     scv.pp.normalize_per_cell(adata, enforce = True)
     scv.pp.filter_genes_dispersion(adata, n_top_genes = 2000)
     scv.pp.log1p(adata)
-    scv.pp.filter_and_normalize(adata, min_shared_counts = 20, n_top_genes = 2000)
+    scv.pp.filter_and_normalize(adata, min_shared_counts = 20, n_top_genes = 2000, enforce = True)
     scv.pp.moments(adata, n_pcs = 30, n_neighbors = 30)
 
     # estimate RNA velocity
     scv.tl.velocity(adata)
     scv.tl.velocity_graph(adata)
+    plt.savefig(fname = fig_path + "velocity_graph_normal" + str(i) + ".png")
 
     # visualize RNA velocity
     scv.pl.velocity_embedding_stream(adata, basis = "UMAP")
     scv.pl.velocity_embedding(adata, arrow_length = 3, arrow_size = 2, dpi = 120)
-
+    plt.savefig(fname = fig_path + "velocity_umap_normal" + str(i) + ".png")
