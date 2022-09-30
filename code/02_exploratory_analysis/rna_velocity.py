0749653ea1dafe86d8a852b16df7f4911822ce4a
Joël Meili <joel.meili@uzh.ch>
2022-09-30 14:32:01 +0200

    updates

--- code/02_exploratory_analysis/rna_velocity.py
+++ code/02_exploratory_analysis/rna_velocity.py
@@ -1,9 +1,15 @@
 import scvelo as scv
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use("AGG")
+
 scv.logging.print_version()
 
 scv.settings.verbosity = 3
 scv.settings.presenter_view = True
 scv.set_figure_params("scvelo")
+
+fig_path = "figures/kidney_mouse/"
 
 for i in range(1, 2):
     adata = scv.read("kidney_mouse/03_data/adata_normal" + str(i) + ".h5ad", cache = True)
@@ -11,6 +17,7 @@ for i in range(1, 2):
 
     # show spliced and unspliced proportion
     scv.pl.proportions(adata)
+    plt.savfig(filename = fig_path + "proportions_normal" + str(i) + ".png")
 
     # basic preprocessing
     scv.pp.filter_and_normalize(adata)
