2bfc0597ca1aaab060ad09a80ce8519d9615defa
Joël Meili <joel.meili@gmail.com>
2022-10-01 18:54:17 +0200

    merged results

--- code/04_differential_analysis/DA_visualize.R
+++ code/04_differential_analysis/DA_visualize.R
@@ -18,7 +18,7 @@ results_brie <- read_results_brie(CLUSTERS = c("Adipocytes"))
 
 
 # merge results from eisaR and BRIE2
-
+results_merged <- merge(results_eisar, results_brie, by = c("Gene_id", "Cell_type"))
 
 # show upset plots
 
--- code/04_differential_analysis/plot_performance.R
+++ code/04_differential_analysis/plot_performance.R
@@ -7,8 +7,9 @@ suppressPackageStartupMessages({
 # read results BRIE2
 read_results_brie <- function (CLUSTERS) {
 	RESULTS <- lapply(CLUSTERS, function (CLUSTER) {
-		res <- read.table(file = paste0("kidney_mouse/03_data/BRIE2/simulation/isA_", CLUSTER, ".brie_ident.tsv"), sep = "\t")
+		res <- read.table(file = paste0("kidney_mouse/03_data/BRIE2/simulation/isA_", CLUSTER, ".brie_ident.tsv"), sep = "\t", header = TRUE)
 		res$Cell_type <- CLUSTER
+		colnames(res)[1] <- "Gene_id"
 		return (res)
 	})
 	
