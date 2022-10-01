170c9cabaf369213d404391b419b418310b3b7a9
Joël Meili <joel.meili@gmail.com>
2022-10-01 19:27:38 +0200

    prepared code for visualization

--- code/04_differential_analysis/DA_visualize.R
+++ code/04_differential_analysis/DA_visualize.R
@@ -13,18 +13,24 @@ source("code/04_differential_analysis/plot_performance.R")
 sce <- readRDS("kidney_mouse/03_data/mouse_simulation_data.rds")
 sce_DGE <- readRDS("kidney_mouse/03_data/mouse_simulation_DGE_data.rds")
 truth <- metadata(sce)$truth
+truth_DGE <- metadata(sce_DGE)$truth
+truth_DGE$truth_union <- ifelse(truth_DGE$truth == 0, ifelse(truth_DGE$truth_DGE == 1, 1, 0), truth_DGE$truth)
 
 # load results from eisaR
 results_eisar <- readRDS(file = "kidney_mouse/03_data/eisar_results.rds")
 results_eisar_DGE <- readRDS(file = "kidney_mouse/03_data/eisar_results_DGE.rds")
 
 # load results from BRIE2
-results_brie <- read_results_brie(CLUSTERS = c("Adipocytes"))
-
+CLUSTERS <- c("Adipocytes")
+results_brie <- read_results_brie(CLUSTERS)
+results_brie_DGE <- read_results_brie(CLUSTERS, DGE = TRUE)
 
 # merge results from eisaR and BRIE2
 results_merged <- merge(results_eisar, results_brie, by = c("Gene_id", "Cell_type"))
 results_merged <- merge(results_merged, truth, by = c("Gene_id", "Cell_type"))
+
+results_merged_DGE <- merge(results_eisar_DGE, results_brie_DGE, by = c("Gene_id", "Cell_type"))
+results_merged_DGE <- merge(results_merged_DGE, truth_DGE, by = c("Gene_id", "Cell_type"))
 
 # show ROC and TPR/FPR curves
 SAVE_FILE_PATH <- "figures/simulation/initial_simulation"
@@ -36,5 +42,17 @@ DF_COBRA <- COBRAData(pval = data.frame(
 		eisaR = results_merged$p_eisaR_adj,
 		BRIE2 = results_merged$is_A_FDR),
 	truth = data.frame(status = results_merged$truth))
+
+plot_performance(DF_COBRA, SAVE_FILE_PATH)
+
+SAVE_FILE_PATH <- "figures/simulation/initial_simulation_DGE"
+
+DF_COBRA <- COBRAData(pval = data.frame(
+	eisaR = results_merged_DGE$p_eisaR,
+	BRIE2 = results_merged_DGE$is_A_pval),
+	padj = data.frame(
+		eisaR = results_merged_DGE$p_eisaR_adj,
+		BRIE2 = results_merged_DGE$is_A_FDR),
+	truth = data.frame(status = results_merged_DGE$truth_union))
 
 plot_performance(DF_COBRA, SAVE_FILE_PATH)
--- code/04_differential_analysis/plot_performance.R
+++ code/04_differential_analysis/plot_performance.R
@@ -5,14 +5,19 @@ suppressPackageStartupMessages({
 })
 
 # read results BRIE2
-read_results_brie <- function (CLUSTERS) {
+read_results_brie <- function (CLUSTERS, DGE) {
 	RESULTS <- lapply(CLUSTERS, function (CLUSTER) {
-		res <- read.table(file = paste0("kidney_mouse/03_data/BRIE2/simulation/isA_", CLUSTER, ".brie_ident.tsv"), sep = "\t", header = TRUE)
-		res$Cell_type <- CLUSTER
-		colnames(res)[1] <- "Gene_id"
+		if (DGE) {
+			res <- read.table(file = paste0("kidney_mouse/03_data/BRIE2/simulation_DGE/isA_", CLUSTER, ".brie_ident.tsv"), sep = "\t", header = TRUE)
+			res$Cell_type <- CLUSTER
+			colnames(res)[1] <- "Gene_id"
+		} else {
+			res <- read.table(file = paste0("kidney_mouse/03_data/BRIE2/simulation/isA_", CLUSTER, ".brie_ident.tsv"), sep = "\t", header = TRUE)
+			res$Cell_type <- CLUSTER
+			colnames(res)[1] <- "Gene_id"
+		}
 		return (res)
 	})
-	
 	return (do.call("rbind", RESULTS))
 }
 
@@ -20,12 +25,13 @@ read_results_brie <- function (CLUSTERS) {
 plot_performance <- function (COBRA_DF, SAVE_FILE_PATH) {
 	
 	perf <- calculate_performance(DF_COBRA, binary_truth = "status")
-	cobra_plot <- prepare_data_for_plot(perf, facetted = TRUE)
+	cobra_plot <- prepare_data_for_plot(perf, facetted = FALSE)
 	
 	# plot ROC curve
-	roc <- plot_roc(cobra_plot) + ggtitle("ROC curve")
+	roc <- plot_roc(cobra_plot, title = "ROC curve")
 	ggsave(roc, filename = paste0(SAVE_FILE_PATH, "_ROC.pdf"))
 	
-	# plot FDR/TPR curve
-	fdr <- plot_fdrtprcurve(cobra_plot) + ggtitle("TPR-FPR plot")
-	ggsave(fdr, filename = paste0(SAVE_FILE_PATH, "_fdr_tpr.pdf"))}
+	# plot TPR/FDR curve
+	fdr <- plot_fdrtprcurve(cobra_plot, title = "TPR-FDR plot")
+	ggsave(fdr, filename = paste0(SAVE_FILE_PATH, "_fdr_tpr.pdf"))
+}
