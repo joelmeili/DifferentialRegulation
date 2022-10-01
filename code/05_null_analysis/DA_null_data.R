e8032deddbba62aa8d9530e63ea47db5080207e0
Joël Meili <joel.meili@gmail.com>
2022-10-01 19:50:43 +0200

    minor changes

--- code/05_null_analysis/DA_null_data.R
+++ code/05_null_analysis/DA_null_data.R
@@ -2,12 +2,24 @@
 rm(list = ls())
 
 # load functions
-source("code/03_differential_analysis/run_differential_analysis.R")
-source("code/03_differential_analysis/prepare_brie.R")
+source("code/04_differential_analysis/run_differential_analysis.R")
+source("code/04_differential_analysis/prepare_brie.R")
 
 # load data
 sce <- readRDS("kidney_mouse/03_data/mouse_data_fry_USA.rds")
-sce_50 <- readRDS("kidney_mouse/03_data/mouse_data_fry_50_50.rds")
+
+# convert sce from USA to US mode
+convert_USA_to_US <- function (sce_USA) {
+	temp <- sce_USA
+	assay(temp, "spliced") <- assay(temp, "spliced") + 0.5 * assay(temp, "ambiguous")
+	assay(temp, "unspliced") <- assay(temp, "unspliced") + 0.5 * assay(temp, "ambiguous")
+	assay(temp, "ambiguous") <- NULL
+	assay(temp, "TOT_counts") <- assay(temp, "spliced") + assay(temp, "unspliced")
+	
+	return (temp)
+}
+
+sce_US <- convert_USA_to_US(sce)
 
 # run DA on the mouse data
 min_count <- 20
@@ -20,7 +32,7 @@ CLUSTERS <- sort(unique(sce$cell_type))[table(sce$cell_type) >= 100]
 
 # run eisaR on the US count data
 RESULTS_EISAR <- lapply(GROUPS, function (GROUP) {
-	run_analysis_eisar(sce = sce_50, GROUP = GROUP, CLUSTERS = CLUSTERS)
+	run_analysis_eisar(sce = sce_US, GROUP = GROUP, CLUSTERS = CLUSTERS)
 })
 
 eisar_RES <- list(RESULTS_EISAR[[1]], RESULTS_EISAR[[2]], RESULTS_EISAR[[3]])
@@ -35,6 +47,3 @@ dexseq_RES <- list(RESULTS_DEXSEQ[[1]], RESULTS_DEXSEQ[[2]], RESULTS_DEXSEQ[[3]]
 # save results
 saveRDS(eisaR_RES, file = "kidney_mouse/03_data/eisar_res_US.rds")
 saveRDS(dexseq_RES, file = "kidney_mouse/03_data/dexseq_res_USA.rds")
-
-# prepare BRIE
-prepare_brie(sce_50)
