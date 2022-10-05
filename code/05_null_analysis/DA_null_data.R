78b367a3dab501644e79789a8825c36125d3b1b4
Joël Meili <joel.meili@uzh.ch>
2022-10-05 12:51:45 +0200

    updated the null analysis code

--- code/05_null_analysis/DA_null_data.R
+++ code/05_null_analysis/DA_null_data.R
@@ -6,7 +6,7 @@ source("code/04_differential_analysis/run_differential_analysis.R")
 source("code/04_differential_analysis/prepare_brie.R")
 
 # load data
-sce <- readRDS("kidney_mouse/03_data/mouse_data_fry_USA.rds")
+sce_USA <- readRDS("kidney_mouse/03_data/mouse_data_fry_USA.rds")
 
 # convert sce from USA to US mode
 convert_USA_to_US <- function (sce_USA) {
@@ -19,7 +19,7 @@ convert_USA_to_US <- function (sce_USA) {
 	return (temp)
 }
 
-sce_US <- convert_USA_to_US(sce)
+sce_US <- convert_USA_to_US(sce_USA)
 
 # run DA on the mouse data
 min_count <- 20
@@ -37,13 +37,13 @@ RESULTS_EISAR <- lapply(GROUPS, function (GROUP) {
 
 eisar_RES <- list(RESULTS_EISAR[[1]], RESULTS_EISAR[[2]], RESULTS_EISAR[[3]])
 
-# run DEXSeq on the USA count data
+# run DEXSeq on the US count data
 RESULTS_DEXSEQ <- lapply(GROUPS, function (GROUP) {
-	run_analysis_dexseq(sce = sce, GROUP = GROUP, CLUSTERS = CLUSTERS, min_count = min_count)
+	run_analysis_dexseq(sce = sce_US, GROUP = GROUP, CLUSTERS = CLUSTERS, min_count = min_count)
 })
 
 dexseq_RES <- list(RESULTS_DEXSEQ[[1]], RESULTS_DEXSEQ[[2]], RESULTS_DEXSEQ[[3]])
 
 # save results
-saveRDS(eisar_RES, file = "kidney_mouse/03_data/eisar_res_US.rds")
-saveRDS(dexseq_RES, file = "kidney_mouse/03_data/dexseq_res_USA.rds")
+saveRDS(eisar_RES, file = "kidney_mouse/03_data/eisar_res_null.rds")
+saveRDS(dexseq_RES, file = "kidney_mouse/03_data/dexseq_res_US_null.rds")
