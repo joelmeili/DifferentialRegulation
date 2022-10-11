dbbf8e003eec46b138563b63abd1e4e16532a875
Joël Meili <joel.meili@uzh.ch>
2022-10-11 22:53:19 +0200

    minor changes

--- code/05_null_analysis/benchmark_visualize.R
+++ code/05_null_analysis/benchmark_visualize.R
@@ -13,13 +13,13 @@ brie_time <- readRDS("kidney_mouse/03_data/brie_bench.rds")
 
 # visualize computational benchmark
 bench_mark <- data.frame(Method = c("BRIE2", "eisaR", "DEXSeq"),
-												 Time = c(3, mean(unlist(eisar_time)),
+												 Time = c(mean(unlist(brie_time)), mean(unlist(eisar_time)),
 												 				 mean(unlist(dexseq_time))))
 
 g1 <- ggplot(bench_mark, aes(x = Method, y = Time)) +
 	geom_bar(stat = "identity") + scale_y_log10() +
 	theme_classic() + ylab("Time in seconds") +
-	theme(axis.text.x = element_text(angle = 45, vjust = 0)) +
+	theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+	stat_identity(geom = "text", colour = "white", size = 2.5, aes(label = paste(round(Time, digits = 2), "seconds")), position = position_stack(vjust = 0.5)) +
 	ggtitle("Computational benchmark on the runtime of the differential methods")
-g1
-
+ggsave(g1, filename = "figures/null_analysis/bench_mark.png", height = 4, width = 6, scale = 1.5)
