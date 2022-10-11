4f4d33615c8b572e64d0fe191d1308e53b97b7d5
Joël Meili <joel.meili@uzh.ch>
2022-10-11 21:03:21 +0200

    minor changes

--- code/05_null_analysis/DA_null_benchmark.R
+++ code/05_null_analysis/DA_null_benchmark.R
@@ -19,7 +19,9 @@ sce_USA <- readRDS("kidney_mouse/03_data/mouse_data_fry_USA.rds")
 # run DA on the mouse data
 min_count <- 20
 
-GROUP <- c("A", "A", "B", "B")
+GROUP <- list(c("A", "A", "B", "B"),
+							c("A", "B", "A", "B"),
+							c("A", "B", "B", "A"))
 
 CLUSTERS <- c("Adipocytes", "Epithelial cells", "Hepatocytes")
 
@@ -30,56 +32,60 @@ cl <- makeCluster(3)
 registerDoParallel(cl, cores = 3)
 
 # run eisaR on the US count data
-start <- Sys.time()
-run_analysis_eisar(sce = sce_US, GROUP = GROUP, CLUSTERS = CLUSTERS, min_count = min_count)
-end <- Sys.time()
-eisar_time <- end - start
+eisar_time <- lapply(GROUPS, function (GROUP)) {
+	start <- Sys.time()
+	run_analysis_eisar(sce = sce_US, GROUP = GROUP, CLUSTERS = CLUSTERS, min_count = min_count)
+	end <- Sys.time()
+	return (end - start)
+}
+saveRDS(eisar_time, "kidney_mouse/03_data/eisar_bench.rds")
+
+# run DEXSeq on the USA count data
+dexseq_time <- lapply(GROUPS, function (GROUP)) {
+	start <- Sys.time()
+	run_analysis_dexseq(sce = sce_US, GROUP = GROUP, CLUSTERS = CLUSTERS, min_count = min_count, method = "USA")
+	end <- Sys.time()
+	return (end - start)
+}
+saveRDS(dexseq_time, "kidney_mouse/03_data/dexseq_bench.rds")
 
 # run BRIE2 on the US count data
-for (i in 1:length(CLUSTERS)) {
-	# filter based on cell type
-	sce_temp <- sce_US[, sce_US$cell_type == CLUSTERS[[i]]]
-	
-	cl <- sapply(strsplit(CLUSTERS[[i]], " "), function (x) paste0(x, collapse = "_"))
-	
-	# set group attribute
-	sce_temp$group <- ifelse(sce_temp$sample_id %in% which(GROUP == "A"), "A", "B")
-	
-	# remove lowly expressed genes: at least 10 non-zero cells:
-	filter <- (rowSums(assays(sce_temp)$TOT_counts[, sce_temp$group == "A"] > 0) >= min_count) & 
-		(rowSums(assays(sce_temp)$TOT_counts[, sce_temp$group == "B"] > 0) >= min_count)
-	
-	sce_temp <- sce_temp[filter, ]
-	
-	ad <- AnnData(
-		X = t(assay(sce_temp, "spliced")),
-		layers = list(
-			spliced = t(assay(sce_temp, "spliced")),
-			unspliced = t(assay(sce_temp, "unspliced"))
+brie_time <- lapply(GROUPS, function (GROUP) {
+	start <- Sys.time()
+	for (i in 1:length(CLUSTERS)) {
+		# filter based on cell type
+		sce_temp <- sce_US[, sce_US$cell_type == CLUSTERS[[i]]]
+		
+		cl <- sapply(strsplit(CLUSTERS[[i]], " "), function (x) paste0(x, collapse = "_"))
+		
+		# set group attribute
+		sce_temp$group <- ifelse(sce_temp$sample_id %in% which(GROUP == "A"), "A", "B")
+		
+		# remove lowly expressed genes: at least 10 non-zero cells:
+		filter <- (rowSums(assays(sce_temp)$TOT_counts[, sce_temp$group == "A"] > 0) >= min_count) & 
+			(rowSums(assays(sce_temp)$TOT_counts[, sce_temp$group == "B"] > 0) >= min_count)
+		
+		sce_temp <- sce_temp[filter, ]
+		
+		ad <- AnnData(
+			X = t(assay(sce_temp, "spliced")),
+			layers = list(
+				spliced = t(assay(sce_temp, "spliced")),
+				unspliced = t(assay(sce_temp, "unspliced"))
+			)
 		)
-	)
-	ad$write_h5ad(paste0("kidney_mouse/03_data/null/BRIE2/cell_info_",
-											 cl, ".h5ad"))
-	write.table(data.frame(index = colnames(sce_temp), 
-												 is_A = ifelse(sce_temp$group == "A", 1, 0)),
-							file = paste0("kidney_mouse/03_data/null/BRIE2/cell_info_", 
+		ad$write_h5ad(paste0("kidney_mouse/03_data/null/BRIE2/cell_info_",
+												 cl, ".h5ad"))
+		write.table(data.frame(index = colnames(sce_temp), 
+													 is_A = ifelse(sce_temp$group == "A", 1, 0)),
+								file = paste0("kidney_mouse/03_data/null/BRIE2/cell_info_", 
 															cl, ".tsv"),
 								quote = FALSE,
 								sep = "\t", row.names = FALSE)
-}
-start <- Sys.time()
-#system("bash code/05_null_analysis/DA_brie.sh")
-end <- Sys.time()
-brie_time <- end - start
-
-# run DEXSeq on the USA count data
-start <- Sys.time()
-run_analysis_dexseq(sce = sce_USA, GROUP = GROUP, CLUSTERS = CLUSTERS, min_count = min_count, method = "USA")
-end <- Sys.time()
-dexseq_time <- end - start
-
-# run DR on the USA data
-pb_counts <- compute_PB_counts(sce_USA[, sce_USA$cell_type %in% CLUSTERS],
-															 EC_list = NULL, design = data.frame(sample = paste0("normal", 1:4),
-															 																		group = GROUP))
-
+	}
+	#system("bash code/05_null_analysis/DA_brie.sh")
+	end <- Sys.time()
+	
+	return (end - start)
+})
+saveRDS(dexseq_time, "kidney_mouse/03_data/brie_bench.rds")
--- code/05_null_analysis/benchmark_visualize.R
+++ code/05_null_analysis/benchmark_visualize.R
@@ -5,3 +5,15 @@ rm(list = ls())
 suppressPackageStartupMessages({
 	library(ggplot2)
 })
+
+# visualize computational benchmark
+bench_mark <- data.frame(Method = c("BRIE2", "eisaR"),
+												 Time = c(1.31*24*60*60, 26))
+
+g1 <- ggplot(bench_mark, aes(x = Method, y = Time)) +
+	geom_bar(stat = "identity") + scale_y_log10() +
+	theme_classic() + ylab("Time in seconds") +
+	theme(axis.text.x = element_text(angle = 45, vjust = 0)) +
+	ggtitle("Computational benchmark on the runtime of the differential methods")
+g1
+
