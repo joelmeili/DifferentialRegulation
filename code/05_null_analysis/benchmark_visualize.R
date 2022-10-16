291ca1949181ab3ac6b5249de0ba420ce9493325
Joël Meili <joel.meili@gmail.com>
2022-10-16 19:52:37 +0200

    removed double pages in report

--- code/05_null_analysis/benchmark_visualize.R
+++ code/05_null_analysis/benchmark_visualize.R
@@ -12,14 +12,13 @@ dexseq_time <- readRDS("kidney_mouse/03_data/dexseq_bench.rds")
 brie_time <- readRDS("kidney_mouse/03_data/brie_bench.rds")
 
 # visualize computational benchmark
-bench_mark <- data.frame(Method = c("BRIE2", "eisaR", "DEXSeq"),
-												 Time = c(mean(unlist(brie_time)), mean(unlist(eisar_time)),
-												 				 mean(unlist(dexseq_time))))
+bench_mark <- data.frame(Method = c("BRIE2", "DEXSeq", "eisaR"),
+												 Time = c(mean(unlist(brie_time))/60, mean(unlist(dexseq_time))/60,
+												 				 mean(unlist(eisar_time))/60))
 
 g1 <- ggplot(bench_mark, aes(x = Method, y = Time)) +
-	geom_bar(stat = "identity") + scale_y_log10() +
-	theme_classic() + ylab("Time") +
+	geom_bar(stat = "identity") + scale_y_sqrt() + theme_classic() + ylab("Time (sqrt-scaled)") +
 	theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-	stat_identity(geom = "text", colour = "white", size = 2.5, aes(label = paste(round(Time, digits = 2), "seconds")), position = position_stack(vjust = 0.5)) +
-	ggtitle("Computational benchmark on the runtime of the differential methods")
-ggsave(g1, filename = "figures/null_analysis/bench_mark.png", height = 4, width = 6, scale = 1.5)
+	stat_identity(geom = "text", colour = "white", size = 2.5, aes(label = paste(round(Time, digits = 2), "minutes")), position = position_stack(vjust = 0.5)) +
+	ggtitle("Average runtime of the differential methods on the Null data set on all three group separations")
+ggsave(g1, filename = "figures/null_analysis/bench_mark.png", height = 3, width = 6, scale = 1.5)
