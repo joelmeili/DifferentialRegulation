d94d3e91014a44febb5754bcec77bad6279401f3
Joël <33486534+joelmeili@users.noreply.github.com>
2022-10-16 21:03:45 +0200

    Update download_human_reads.sh

--- code/01_annotation/download_human_reads.sh
+++ code/01_annotation/download_human_reads.sh
@@ -4,5 +4,5 @@ human_files=(SRR8869247 SRR8869248 SRR8869249 SRR8869262 SRR8869263 SRR8869264)
 
 for file in ${human_files[@]}
 do
-	prefetch ${file} --output-directory brain_human/fastq && fasterq-dump ${file} -O brain_human/fastq
+	prefetch ${file} --output-directory brain_human/fastq && fasterq-dump brain_human/fastq/${file} -O brain_human/fastq
 done
