ec76131d9b7aaab07b0d481ecdfdf9b42842c0b9
Joël Meili <joel.meili@gmail.com>
2022-10-20 14:13:11 +0200

    changed benchmark from USA mode to EC mode

--- code/05_null_analysis/DA_null_benchmark.R
+++ code/05_null_analysis/DA_null_benchmark.R
@@ -31,70 +31,85 @@ sce_US <- convert_USA_to_US(sce_USA)
 cl <- makeCluster(3)
 registerDoParallel(cl, cores = 3)
 
-# run eisaR on the US count data
-eisar_time <- lapply(GROUPS, function (GROUP) {
-	start <- Sys.time()
-	run_analysis_eisar(sce = sce_US, GROUP = GROUP, CLUSTERS = CLUSTERS, min_count = min_count)
-	end <- Sys.time()
-	return (as.numeric(end - start, units = "secs"))
-})
-saveRDS(eisar_time, "kidney_mouse/03_data/eisar_bench.rds")
+# # run eisaR on the US count data
+# eisar_time <- lapply(GROUPS, function (GROUP) {
+# 	start <- Sys.time()
+# 	run_analysis_eisar(sce = sce_US, GROUP = GROUP, CLUSTERS = CLUSTERS, min_count = min_count)
+# 	end <- Sys.time()
+# 	return (as.numeric(end - start, units = "secs"))
+# })
+# saveRDS(eisar_time, "kidney_mouse/03_data/eisar_bench.rds")
+# 
+# # run DEXSeq on the USA count data
+# dexseq_time <- lapply(GROUPS, function (GROUP) {
+# 	start <- Sys.time()
+# 	run_analysis_dexseq(sce = sce_USA, GROUP = GROUP, CLUSTERS = CLUSTERS, min_count = min_count, method = "USA")
+# 	end <- Sys.time()
+# 	return (as.numeric(end - start, units = "secs"))
+# })
+# saveRDS(dexseq_time, "kidney_mouse/03_data/dexseq_bench.rds")
+# 
+# # run BRIE2 on the US count data
+# brie_time <- lapply(GROUPS, function (GROUP) {
+# 	start <- Sys.time()
+# 	for (i in 1:length(CLUSTERS)) {
+# 		# filter based on cell type
+# 		sce_temp <- sce_US[, sce_US$cell_type == CLUSTERS[[i]]]
+# 
+# 		cl <- sapply(strsplit(CLUSTERS[[i]], " "), function (x) paste0(x, collapse = "_"))
+# 
+# 		# set group attribute
+# 		sce_temp$group <- ifelse(sce_temp$sample_id %in% which(GROUP == "A"), "A", "B")
+# 
+# 		# remove lowly expressed genes: at least 10 non-zero cells:
+# 		filter <- (rowSums(assays(sce_temp)$TOT_counts[, sce_temp$group == "A"] > 0) >= min_count) &
+# 			(rowSums(assays(sce_temp)$TOT_counts[, sce_temp$group == "B"] > 0) >= min_count)
+# 
+# 		sce_temp <- sce_temp[filter, ]
+# 
+# 		ad <- AnnData(
+# 			X = t(assay(sce_temp, "spliced")),
+# 			layers = list(
+# 				spliced = t(assay(sce_temp, "spliced")),
+# 				unspliced = t(assay(sce_temp, "unspliced"))
+# 			)
+# 		)
+# 		ad$write_h5ad(paste0("kidney_mouse/03_data/null/BRIE2/cell_info_",
+# 												 cl, ".h5ad"))
+# 		write.table(data.frame(index = colnames(sce_temp),
+# 													 is_A = ifelse(sce_temp$group == "A", 1, 0)),
+# 								file = paste0("kidney_mouse/03_data/null/BRIE2/cell_info_",
+# 															cl, ".tsv"),
+# 								quote = FALSE,
+# 								sep = "\t", row.names = FALSE)
+# 	}
+# 	system("bash code/05_null_analysis/DA_brie.sh")
+# 	end <- Sys.time()
+# 	return (as.numeric(end - start, units = "secs"))
+# })
+# saveRDS(brie_time, "kidney_mouse/03_data/brie_bench.rds")
 
-# run DEXSeq on the USA count data
-dexseq_time <- lapply(GROUPS, function (GROUP) {
-	start <- Sys.time()
-	run_analysis_dexseq(sce = sce_USA, GROUP = GROUP, CLUSTERS = CLUSTERS, min_count = min_count, method = "USA")
-	end <- Sys.time()
-	return (as.numeric(end - start, units = "secs"))
-})
-saveRDS(dexseq_time, "kidney_mouse/03_data/dexseq_bench.rds")
+# run DR on the equivalence classes
+sample_ids <- paste0("normal", 1:4)
+base_dir <- file.path("kidney_mouse/02_data_fry/", sample_ids)
+path_to_EC_counts <- file.path(base_dir, "/alevin/geqc_counts.mtx")
+path_to_EC <- file.path(base_dir, "/alevin/gene_eqclass.txt.gz")
+path_to_cell_id <- file.path(base_dir,"/alevin/quants_mat_rows.txt")
+path_to_gene_id <- file.path(base_dir,"/alevin/quants_mat_cols.txt")
 
-# run BRIE2 on the US count data
-brie_time <- lapply(GROUPS, function (GROUP) {
-	start <- Sys.time()
-	for (i in 1:length(CLUSTERS)) {
-		# filter based on cell type
-		sce_temp <- sce_US[, sce_US$cell_type == CLUSTERS[[i]]]
+EC_list <- load_EC(path_to_EC_counts = path_to_EC_counts,
+									 path_to_EC = path_to_EC,
+									 path_to_cell_id = path_to_cell_id,
+									 path_to_gene_id = path_to_gene_id,
+									 sample_ids = sample_ids)
 
-		cl <- sapply(strsplit(CLUSTERS[[i]], " "), function (x) paste0(x, collapse = "_"))
 
-		# set group attribute
-		sce_temp$group <- ifelse(sce_temp$sample_id %in% which(GROUP == "A"), "A", "B")
-
-		# remove lowly expressed genes: at least 10 non-zero cells:
-		filter <- (rowSums(assays(sce_temp)$TOT_counts[, sce_temp$group == "A"] > 0) >= min_count) &
-			(rowSums(assays(sce_temp)$TOT_counts[, sce_temp$group == "B"] > 0) >= min_count)
-
-		sce_temp <- sce_temp[filter, ]
-
-		ad <- AnnData(
-			X = t(assay(sce_temp, "spliced")),
-			layers = list(
-				spliced = t(assay(sce_temp, "spliced")),
-				unspliced = t(assay(sce_temp, "unspliced"))
-			)
-		)
-		ad$write_h5ad(paste0("kidney_mouse/03_data/null/BRIE2/cell_info_",
-												 cl, ".h5ad"))
-		write.table(data.frame(index = colnames(sce_temp),
-													 is_A = ifelse(sce_temp$group == "A", 1, 0)),
-								file = paste0("kidney_mouse/03_data/null/BRIE2/cell_info_",
-															cl, ".tsv"),
-								quote = FALSE,
-								sep = "\t", row.names = FALSE)
-	}
-	system("bash code/05_null_analysis/DA_brie.sh")
-	end <- Sys.time()
-	return (as.numeric(end - start, units = "secs"))
-})
-saveRDS(brie_time, "kidney_mouse/03_data/brie_bench.rds")
-
-# run DR on the USA count data
 dr_time <- lapply(GROUPS, function (GROUP) {
 	start <- Sys.time()
 	
 	# set temporary SCE
 	sce_temp <- sce_USA[, sce_USA$cell_type %in% CLUSTERS]
+	colnames(sce_temp) <- paste0("normal", colnames(sce_temp))
 	
 	# set group attribute
 	sce_temp$group <- ifelse(sce_temp$sample_id %in% which(GROUP == "A"), "A", "B")
@@ -106,11 +121,11 @@ dr_time <- lapply(GROUPS, function (GROUP) {
 	sce_temp$sample_id <- as.factor(paste0("normal", sce_temp$sample_id))
 	
 	# calculate pb counts
-	pb_counts <- compute_PB_counts(sce = sce_temp, EC_list = NULL, 
+	pb_counts <- compute_PB_counts(sce = sce_temp, EC_list = EC_list, 
 																 design = data.frame(sample = paste0("normal", 1:4),
 																 										group = GROUP))
 	# run DR
-	DifferentialRegulation(pb_counts, EC = FALSE, n_cores = 3)
+	DifferentialRegulation(pb_counts, n_cores = 3)
 	end <- Sys.time()
 	return (as.numeric(end - start, units = "secs"))
 })
